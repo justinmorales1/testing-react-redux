b3742976381aaa8ff7ba6907df641b3e48a04885
justinmorales1 <jm.em0210@gmail.com>
2021-08-18 10:59:25 -0500

    Adding unmount for commentbox tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,6 +9,10 @@ beforeEach(() => {
   component = mount(<CommentBox />);
 }) 
 
+afterEach(() => {
+  component.unmount();
+})
+
 it('has a text area and a button', () => {
   expect(component.find("textarea").length).toEqual(1);
   expect(component.find("button").length).toEqual(1);
