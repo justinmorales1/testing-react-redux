24ff32d4475c3307a9f0c0e1122b2d883fefdcf0
justinmorales1 <jm.em0210@gmail.com>
2021-08-18 10:33:46 -0500

    Adding absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+
 
 it('shows a comment box',() => {
-  const div = document.createElement('div');
+  const component = shallow(<App/>);
 
-  ReactDOM.render(<App />, div);
+  expect(component.find(CommentBox).length).toEqual(1);
 
-  console.log(div.innerHTML)
 
-  expect(div.innerHTML).toContain('Comment Box');
+})
 
-  ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list',() => {
+  const component = shallow(<App/>);
+
+console.log(component.find(CommentList).debug)
+
+  expect(component.find(CommentList).length).toEqual(1);
+
+
 })
